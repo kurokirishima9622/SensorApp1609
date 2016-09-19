8882298d6340cc23adf8829a1e2cc4e3b9017586
kurokirishima9622 <rs960222@gmail.com>
2016-09-20 04:10:58 +0900

    初期コミット

---
+++ app/src/main/java/jp/ac/hal/st32_kadai06/MainActivity.java
@@ -0,0 +1,13 @@
+package jp.ac.hal.st32_kadai06;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
